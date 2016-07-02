150f310935a5eadbff9dfd9e6d6bda6cc1c7713c
Doug <dlm2dh@gmail.com>
2016-07-01 21:34:20 -0400

    Getting message components working

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -2,7 +2,29 @@ var MessageBox = React.createClass({
   render : function() {
     return (
       <div className="messageBox">
-        Hello, world! I am a MessageBox.
+        <h1>Message</h1>
+        <MessageList />
+        <MessageForm />
+      </div>
+    );
+  }
+});
+
+var MessageList = React.createClass({
+  render : function() {
+    return (
+      <div className="messageList">
+        Hello, world! I am a messageList.
+      </div>
+    );
+  }
+});
+
+var MessageForm = React.createClass({
+  render : function() {
+    return (
+      <div className="messageForm">
+        Hello, world! I am a MessageForm.
       </div>
     );
   }
